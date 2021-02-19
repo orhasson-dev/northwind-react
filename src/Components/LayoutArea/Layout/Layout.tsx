d6111227c9f63589e88c8f469830a50d845ba035
Or Hasson <hassonor@gmail.com>
2021-02-19 11:43:42 +0200

    set Home Content.

--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -2,13 +2,14 @@ import './Layout.css';
 import Header from '../Header/Header';
 import Footer from "../Footer/Footer";
 import Menu from "../Menu/Menu";
+import Home from "../../HomeArea/Home/Home";
 
 function Layout() : JSX.Element { // JSX.Element = UI object
     return (
         <div className="Layout">
             <header><Header/></header>
             <aside><Menu/></aside>
-            <main>C</main>
+            <main><Home/></main>
             <footer><Footer/></footer>
         </div>
     );
