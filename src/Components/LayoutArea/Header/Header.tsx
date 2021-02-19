c3bda5575e0519d7ac00d713801c90c48d6ffcb1
Or Hasson <hassonor@gmail.com>
2021-02-19 10:56:40 +0200

    fix Header component with style.

--- src/Components/LayoutArea/Header/Header.tsx
+++ src/Components/LayoutArea/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() : JSX.Element { // JSX.Element = UI object
     return (
         <div className="Header">
-
+            <h1>Northwind Website</h1>
         </div>
     );
 }
--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import './Layout.css';
+import React from 'react';
+import Header from '../Header/Header';
 
 function Layout() : JSX.Element { // JSX.Element = UI object
     return (
         <div className="Layout">
-            <header>A</header>
+            <header><Header/></header>
             <aside>B</aside>
             <main>C</main>
             <footer>D</footer>
