335248c1710835dec2bf910db1bfc4457b8ecbc3
Or Hasson <hassonor@gmail.com>
2021-02-19 12:43:46 +0200

    Adding condition rendering examples

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -1,12 +1,20 @@
 import "./Home.css";
 import Discount from "../Discount/Discount";
+import Specials from "../Specials/Specials";
 
 function Home(): JSX.Element {
     return (
         <div className="Home">
+
+            {/*Interpolation:*/}
 			<Discount/>
+
+			{/*Conditional Rendering: */}
+            <Specials/>
         </div>
     );
 }
+
+
 
 export default Home;
---
+++ src/Components/HomeArea/Specials/Specials.tsx
@@ -0,0 +1,27 @@
+import "./Specials.css";
+
+function Specials(): JSX.Element {
+    return (
+        <div className="Specials Box">
+			<p>
+                {isWeekend() ? <span>Pizza</span> : null}
+                <span> | </span>
+
+                {isWeekend() ? <span>Sushi</span> : <span>Pasta</span>}
+
+                <span> | </span>
+
+                { isWeekend() && <span>Cholent</span> }
+
+            </p>
+        </div>
+    );
+}
+
+function isWeekend(): boolean{
+    const nowDate  = new Date();
+    const dayOfWeek = nowDate.getDay() + 1;
+    return dayOfWeek >= 6;
+}
+
+export default Specials;
