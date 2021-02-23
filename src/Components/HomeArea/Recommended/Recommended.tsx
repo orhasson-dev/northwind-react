d21376189e8623921a92863fd23d2bda13297ae4
Or Hasson <hassonor@gmail.com>
2021-02-23 19:58:40 +0200

    Event example class component

--- src/Components/HomeArea/Recommended/Recommended.tsx
+++ src/Components/HomeArea/Recommended/Recommended.tsx
@@ -1,20 +1,50 @@
 import "./Recommended.css";
-import {SyntheticEvent} from "react";
+import {Component, SyntheticEvent} from "react";
 
-function Recommended(): JSX.Element {
 
-    const item = "Red Wine"; //Demo for getting it from api
+class Recommended extends Component{
 
-    function showRecommendation(args: SyntheticEvent):void{
-        console.log(args);
-        alert(item);
+    private item = "Red Wine"; //Demo for getting it from api
+
+    private showRecommendation = (args: SyntheticEvent) => {
+        console.log(args.target); // The element raising the event
+        console.log(args.nativeEvent); // The HTML native event
+        console.log((args.target as HTMLButtonElement).innerHTML); // Converting target to a button for getting the innerHTML
+        alert(this.item);
     }
 
-    return (
+    public render(): JSX.Element {
+        return (
         <div className="Recommended Box">
-	        <button onClick={showRecommendation}>Recommended a Product</button>
+	        <button onClick={this.showRecommendation}>Recommended a Product</button>
         </div>
     );
+    }
 }
+
+
+
+
+
+
+
+//Functional Component Event Example.
+// function Recommended(): JSX.Element {
+//
+//     const item = "Red Wine"; //Demo for getting it from api
+//
+//     function showRecommendation(args: SyntheticEvent):void{
+//         console.log(args.target); // The element raising the event
+//         console.log(args.nativeEvent); // The HTML native event
+//         console.log((args.target as HTMLButtonElement).innerHTML); // Converting target to a button for getting the innerHTML
+//         alert(item);
+//     }
+//
+//     return (
+//         <div className="Recommended Box">
+// 	        <button onClick={showRecommendation}>Recommended a Product</button>
+//         </div>
+//     );
+// }
 
 export default Recommended;
