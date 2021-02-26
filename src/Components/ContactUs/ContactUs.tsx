ddaa3c3a8686eddab7c54c4e8d28f1609528352e
Or Hasson <hassonor@gmail.com>
2021-02-26 12:51:56 +0200

    ContactUs with material-ui styles example

---
+++ src/Components/ContactUs/ContactUs.tsx
@@ -0,0 +1,37 @@
+import "./ContactUs.css";
+import {Button, ButtonGroup, Checkbox, FormControlLabel, makeStyles, TextField, Typography} from "@material-ui/core";
+
+function ContactUs(): JSX.Element {
+
+    const createClasses = makeStyles( {
+        textBox: {margin:"5px 0", width: 400},
+        headline: {fontSize: 30}
+    });
+
+    const classes = createClasses();
+
+    return (
+        <div className="ContactUs Box">
+		    <Typography className={classes.headline}> Contact Us</Typography>
+
+            <TextField label="Name" variant="outlined" className={classes.textBox} />
+            <br />
+
+            <TextField label="Email" variant="outlined"  className={classes.textBox}  />
+            <br />
+
+            <TextField label="Message" variant="outlined"  className={classes.textBox}  />
+            <br />
+
+            <FormControlLabel control={<Checkbox/>} label="Send me updates via email"/>
+            <br/>
+
+            <ButtonGroup variant="contained" fullWidth>
+            <Button color="primary">Send</Button>
+            <Button color="secondary">Cancel</Button>
+            </ButtonGroup>
+        </div>
+    );
+}
+
+export default ContactUs;
--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -3,13 +3,17 @@ import Header from '../Header/Header';
 import Footer from "../Footer/Footer";
 import Menu from "../Menu/Menu";
 import Home from "../../HomeArea/Home/Home";
+import ContactUs from "../../ContactUs/ContactUs";
 
 function Layout(): JSX.Element { // JSX.Element = UI object
     return (
         <div className="Layout">
             <header><Header/></header>
             <aside><Menu/></aside>
-            <main><Home/></main>
+            <main>
+                {/*<Home/>*/}
+                <ContactUs/>
+            </main>
             <footer><Footer/></footer>
         </div>
     );
