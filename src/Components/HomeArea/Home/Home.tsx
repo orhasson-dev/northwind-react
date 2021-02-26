d492c2a449dcf7285c091d02dca3d54bf4353087
Or Hasson <hassonor@gmail.com>
2021-02-26 08:54:13 +0200

    Add Cool Timer2

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -6,9 +6,7 @@ import Sales from "../Sales/Sales";
 import Recommended from "../Recommended/Recommended";
 import Location from "../Location/Location";
 import Bestseller from "../Bestseller/Bestseller";
-import RandomProduct from "../RandomProduct/RandomProduct";
-import Shoes from "../Shoes/Shoes";
-import Timer from "../Timer/Timer";
+
 
 function Home(): JSX.Element {
     return (
@@ -33,18 +31,9 @@ function Home(): JSX.Element {
            <Location/>
            <Bestseller/>
 
-            <Shoes brand="Nike" size={37.5} price ={99}
-                   imagePath="images/20200605-nike-06.jpg"/>
-            <Shoes brand="Adidas" size={38.5} price ={98}
-                   imagePath="images/asics-shoes.jpg"/>
-            <Shoes brand="Puma" size={39.5} price ={97}
-                   imagePath="images/61xpVxIbIVL._UL1200_.jpg"/>
-            <Shoes brand="New Balance" size={40.5} price ={96}
-                   imagePath="images/new_balance_shoes.jpg"/>
-            <Shoes brand="Asics" size={42} price ={95}
-                   imagePath="images/asics-shoes.jpg"/>
-            <RandomProduct/>
-        0
+
+            {/*<RandomProduct/>*/}
+
         </div>
     );
 }
