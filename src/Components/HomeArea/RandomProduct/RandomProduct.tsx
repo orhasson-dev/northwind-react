efde9b0acc3512e856712812f3bf8fad7321eb26
Or Hasson <hassonor@gmail.com>
2021-02-26 09:52:20 +0200

    componentWillUnmount func.

--- src/Components/HomeArea/RandomProduct/RandomProduct.tsx
+++ src/Components/HomeArea/RandomProduct/RandomProduct.tsx
@@ -8,21 +8,24 @@ interface RandomProductState {
 
 class RandomProduct extends Component<{}, RandomProductState> {
 
+    private timerId = 0;
 
     // Constructor - Object has been created
     public constructor(props: {}) {
         super(props);
-        this.state = { products:["shirt","food","drink","iPhone12","Note20"], randomProduct: ""}; // Init the state.
+        this.state = { products:["Sony Z2","Samsung Galaxy 21 Ultra","iPhone 12 Pro Max","iPhone12","Note20"],
+            randomProduct: ""}; // Init the state.
 
     }
 
     // componentDidMount - Object is ready for use:
     public componentDidMount(): void {
-        setInterval(() => {
-            const rand = Math.floor(Math.random() * this.state.products.length);
-            this.setState({randomProduct: this.state.products[rand]})
-        }, 1000)
+        this.timerId = window.setInterval(() => {
+            const index = Math.floor(Math.random() * this.state.products.length);
+            this.setState({ randomProduct: this.state.products[index] });
+        }, 1000);
     }
+
 
 
     public render(): JSX.Element {
@@ -32,6 +35,17 @@ class RandomProduct extends Component<{}, RandomProductState> {
             </div>
         );
     }
+
+    // componentDidUpdate - Component state or props has been changed
+    public componentDidUpdate(): void{
+
+    }
+
+    // componentWillUnmount: the component is about to be destroyed
+    public componentWillUnmount(): void {
+        clearInterval(this.timerId);
+    }
+
 }
 
 
