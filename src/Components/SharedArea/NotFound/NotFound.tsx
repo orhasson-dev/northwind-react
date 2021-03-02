979c2b0459871f65b6cfebd4437d643d952c361a
Or Hasson <hassonor@gmail.com>
2021-03-02 19:46:01 +0200

    adding iframe video

--- src/Components/SharedArea/NotFound/NotFound.tsx
+++ src/Components/SharedArea/NotFound/NotFound.tsx
@@ -4,6 +4,8 @@ function NotFound(): JSX.Element {
     return (
         <div className="NotFound">
 			<p> The Page you are looking for doesn't exist.</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0?autoplay=1" allow="autoplay" title="Page not Found"/>
+
         </div>
     );
 }
