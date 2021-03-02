a39d0476231754ed7688e1c55145978d5c635b5e
Or Hasson <hassonor@gmail.com>
2021-03-02 19:40:29 +0200

    Add NotFound Route

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,18 +1,19 @@
-import {Redirect,BrowserRouter, Route, Switch} from "react-router-dom"
+import {Redirect, Route, Switch} from "react-router-dom"
 import Home from "../../HomeArea/Home/Home";
 import About from "../../AboutArea/About/About";
 import ProductList from "../../ProductArea/ProductList/ProductList";
 import ContactUs from "../../ContactUs/ContactUs";
+import NotFound from "../../SharedArea/NotFound/NotFound";
 
 function Routing(): JSX.Element {
     return (
-
         <Switch>
             <Route path="/home" component={Home} exact/>
             <Route path="/products" component={ProductList} exact/>
             <Route path="/about" component={About} exact/>
             <Route path="/contact-us" component={ContactUs} exact/>
             <Redirect from="/" to="/home" exact/>
+            <Route component={NotFound} />
         </Switch>
     );
 }
---
+++ src/Components/SharedArea/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import "./NotFound.css";
+
+function NotFound(): JSX.Element {
+    return (
+        <div className="NotFound">
+			<p> The Page you are looking for doesn't exist.</p>
+        </div>
+    );
+}
+
+export default NotFound;
