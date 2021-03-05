5a436bc20ddb992a4ae91326885e04b0e7522dcc
Or Hasson <hassonor@gmail.com>
2021-03-05 10:28:11 +0200

    Create Glbals variables class

--- src/Components/ProductArea/ProductList/ProductList.tsx
+++ src/Components/ProductArea/ProductList/ProductList.tsx
@@ -1,9 +1,11 @@
-import { Component } from "react";
+import {Component, Fragment} from "react";
 import "./ProductList.css";
 import ProductModel from "../../../Models/ProductModel";
+import axios from "axios";
+import globals from "../../../Services/Globals";
 
 interface ProductListState {
-	products: ProductModel[];
+    products: ProductModel[];
 }
 
 class ProductList extends Component<{}, ProductListState> {
@@ -11,16 +13,37 @@ class ProductList extends Component<{}, ProductListState> {
     public constructor(props: {}) {
         super(props);
         this.state = {
-			
+            products: []
         };
+    }
+
+
+    // We have to install "npm i -g northwind-back-end"
+    // Then We have to open new terminal and start the northwind server by "northwind" command
+
+    public async componentDidMount() {
+        try {
+            const response = await axios.get<ProductModel[]>(globals.productUrl);
+            this.setState({products: response.data});
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     public render(): JSX.Element {
         return (
             <div className="ProductList">
-				<h2> Products Page </h2>
+                {this.state.products.map((item) =>
+                    <Fragment key={item.id}>
+                        <span>{item.name} | </span>
+                        <span>{item.price} | </span>
+                        <span>{item.stock} | </span>
+                        <span>{item.imageName} </span>
+                        <br/>
+                    </Fragment>
+                )}
             </div>
-        );
+        )
     }
 }
 
---
+++ src/Services/Globals.ts
@@ -0,0 +1,7 @@
+class Globals{
+    public productUrl = "http://localhost:3030/api/products";
+}
+
+const globals = new Globals();
+
+export default globals;
