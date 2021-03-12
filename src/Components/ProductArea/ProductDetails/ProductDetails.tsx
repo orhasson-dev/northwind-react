c7cc265528e59a34dc34962b5e68085eb6b3fc9d
Or Hasson <hassonor@gmail.com>
2021-03-12 09:37:48 +0200

    Add redux package

--- src/Components/ProductArea/ProductDetails/ProductDetails.tsx
+++ src/Components/ProductArea/ProductDetails/ProductDetails.tsx
@@ -1,48 +1,46 @@
-import { Component } from "react";
+import {Component} from "react";
 import "./ProductDetails.css";
 import {NavLink} from "react-router-dom";
 import ProductModel from "../../../Models/ProductModel";
-import { RouteComponentProps } from "react-router-dom";
+import {RouteComponentProps} from "react-router-dom";
 import axios from "axios";
 import globals from "../../../Services/Globals";
 import ProductCard from "../ProductCard/ProductCard";
 import ProgressBar from "../../SharedArea/ProgressBar/ProgressBar";
-import { confirmAlert } from "react-confirm-alert";
+import {confirmAlert} from "react-confirm-alert";
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
 
 // interface containing the route parameters
 // The exact route params in the Routing must be here as string variables:
-interface RouteParams{
+interface RouteParams {
     id: string;
 }
 
 // Our props interface must extends the following:
-interface ProductDetailsProps extends RouteComponentProps<RouteParams>{
+interface ProductDetailsProps extends RouteComponentProps<RouteParams> {
 
 }
 
 // Our State:
 interface ProductDetailsState {
-	product: ProductModel;
+    product: ProductModel;
 }
 
 class ProductDetails extends Component<ProductDetailsProps, ProductDetailsState> {
 
 
-
-
     public constructor(props: ProductDetailsProps) {
         super(props);
         this.state = {
-			product: null
+            product: null
         };
     }
 
     public async componentDidMount() {
         try {
             const response = await axios.get<ProductModel>(
-                globals.productUrlDelayed  + this.props.match.params.id );
+                globals.productUrlDelayed + this.props.match.params.id);
             this.setState({product: response.data});
         } catch (err) {
             console.log(err);
@@ -54,7 +52,7 @@ class ProductDetails extends Component<ProductDetailsProps, ProductDetailsState>
         confirmAlert({
             title: "CAUTION !!!!",
             message:
-                "Are you absolutely sure you want to delete section ",
+                "Are you absolutely sure you want to delete product ",
             buttons: [
                 {
                     label: "Yes",
@@ -62,47 +60,46 @@ class ProductDetails extends Component<ProductDetailsProps, ProductDetailsState>
                 },
                 {
                     label: "No",
-                    onClick: () => {}
+                    onClick: () => {
+                    }
                 }
             ]
         });
+
     };
 
-    private deleteProduct = async ()=>{
+    private deleteProduct = async () => {
 
-        try{
+        try {
             //create a message Do you really want to delete?
-
-
             await axios.delete(globals.productUrl + this.state.product.id);
 
             //Redirect to products page.
             this.props.history.push("/products");
 
-        }
-        catch(err){
+        } catch (err) {
             console.log(err);
         }
-        }
+    }
 
 
     public render(): JSX.Element {
         return (
             <div className="ProductDetails">
                 <h2>Product Details</h2>
-                { this.state.product === null && <ProgressBar /> }
+                {this.state.product === null && <ProgressBar/>}
 
                 {/*Add the loading component until product downloaded from server*/}
                 {this.state.product &&
-                    <>
-                <h3>{this.state.product.name}</h3>
+                <>
+                    <h3>{this.state.product.name}</h3>
                     <h3>Price: ${this.state.product.price}</h3>
                     <h3>Stock: {this.state.product.stock}</h3>
                     <img src={globals.productUrl + "images/" + this.state.product.imageName} alt="View"/>
-                    </>
+                </>
                 }
                 <br/><br/>
-                <NavLink to="/products" > Back </NavLink>
+                <NavLink to="/products"> Back </NavLink>
                 <span> | </span>
                 <a href="#" onClick={this.submitHandler}>Delete</a>
 
